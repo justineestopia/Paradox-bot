291d4a5da5455fdd27ad19116a3e5716522e44ea
justineestopia <justineestopia60@gmail.com>
2026-06-25 22:39:18 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "MTUxOTcwNzk5ODI2MjM5NTEwMw.GH0TnY.arNHVGdmKheMQ_ZowQ3qDN7s_C3rq-1yWjobv8"
+TOKEN = "MTUxOTcwNzk5ODI2MjM5NTEwMw.GAWSTA.qyI8DpjPpUPjgL_BmVXoTbaq_jAVN-3BsFDO-k"
 
 intents = discord.Intents.default()
 intents.message_content = True
